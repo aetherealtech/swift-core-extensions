330eff0f40975d7b502c0ac895e071cd75f6ef01
Dan Coleman <dan@aetherealtech.com>
2024-10-02 21:28:50 -0500

    Throwing future tests

--- Tests/CombineExtensionsTests/AsyncFutureTests.swift
+++ Tests/CombineExtensionsTests/AsyncFutureTests.swift
@@ -4,6 +4,8 @@ import XCTest
 
 @testable import CombineExtensions
 
+struct TestError: Error {}
+
 @available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 final class AsyncFutureTests: XCTestCase {
     @MainActor
@@ -103,6 +105,123 @@ final class AsyncFutureTests: XCTestCase {
             .waitUntilNotNil()!
         
         try assertEqual([0: 0, 1: 1, 2: 2], receivedValues)
+        
+        withExtendedLifetime(subscription1) { }
+        withExtendedLifetime(subscription2) { }
+        withExtendedLifetime(subscription3) { }
+    }
+    
+    @MainActor
+    func testThrowingFuture() async throws {
+        let continuations = CurrentValueSubject<[Int: CheckedContinuation<Void, any Error>], Never>([:])
+        
+        var currentIndex = 0
+        
+        let work = { @Sendable @MainActor in
+            let index = currentIndex
+            
+            currentIndex += 1
+            
+            try await withCheckedThrowingContinuation { continuation in
+                continuations.value[index] = continuation
+            }
+            
+            return index
+        }
+        
+        var receivedValues: [Int: Int] = [:]
+        nonisolated(unsafe) let completions = CurrentValueSubject<[Int: Subscribers.Completion<any Error>], Never>([:])
+        
+        let future = AsyncFuture(work)
+                
+        let subscription1 = future.sink(
+            receiveCompletion: { @Sendable completion in
+                Task { @MainActor in
+                    completions.value[0] = completion
+                }
+            },
+            receiveValue: { @Sendable value in
+                Task { @MainActor in
+                    receivedValues[0] = value
+                }
+            }
+        )
+        
+        let continuation1 = await continuations.values
+            .compactMap { $0[0] }
+            .waitUntilNotNil()!
+        
+        let subscription2 = future.sink(
+            receiveCompletion: { @Sendable completion in
+                Task { @MainActor in
+                    completions.value[1] = completion
+                }
+            },
+            receiveValue: { @Sendable value in
+                Task { @MainActor in
+                    receivedValues[1] = value
+                }
+            }
+        )
+        
+        let continuation2 = await continuations.values
+            .compactMap { $0[1] }
+            .waitUntilNotNil()!
+        
+        let subscription3 = future.sink(
+            receiveCompletion: { @Sendable completion in
+                Task { @MainActor in
+                    completions.value[2] = completion
+                }
+            },
+            receiveValue: { @Sendable value in
+                Task { @MainActor in
+                    receivedValues[2] = value
+                }
+            }
+        )
+        
+        let continuation3 = await continuations.values
+            .compactMap { $0[2] }
+            .waitUntilNotNil()!
+        
+        continuation3.resume()
+
+        let completion3 = await completions.values
+            .compactMap { $0[2] }
+            .waitUntilNotNil()!
+        
+        try assertEqual([2: 2], receivedValues)
+        
+        guard case .finished = completion3 else {
+            throw Fail("Expected a normal publisher completion")
+        }
+        
+        continuation2.resume(throwing: TestError())
+        
+        let completion2 = await completions.values
+            .compactMap { $0[1] }
+            .waitUntilNotNil()!
+        
+        try assertEqual([2: 2], receivedValues)
+        
+        guard case let .failure(error) = completion2 else {
+            throw Fail("Expected a publisher failure")
+        }
+        
+        try assertTrue(error is TestError)
+        
+        continuation1.resume()
+        
+        let completion1 = await completions.values
+            .compactMap { $0[0] }
+            .waitUntilNotNil()!
+        
+        guard case .finished = completion1 else {
+            throw Fail("Expected a normal publisher completion")
+        }
+        
+        try assertEqual([0: 0, 2: 2], receivedValues)
         
         withExtendedLifetime(subscription1) { }
         withExtendedLifetime(subscription2) { }
